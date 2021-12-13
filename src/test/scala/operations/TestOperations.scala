e3e3543e2ebc07e9652b15558fc059200e5b927c
rrazumilovs <razumilovrenat@inbox.lv>
2021-12-13 20:56:19 +0200

    test fix 2

--- src/test/scala/operations/TestOperations.scala
+++ src/test/scala/operations/TestOperations.scala
@@ -1,35 +1,30 @@
 package operations
 
-import com.deloitte.demoApp.operations.Operation
-import com.deloitte.demoApp.operations.operationImpl.{Prod, Sub, Sum}
+import com.deloitte.demoApp.operations.OperationsHandler
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TestOperations {
+  val ops : OperationsHandler = new OperationsHandler()
+
     @Test
     def testSum(): Unit ={
-      implicit val args : Array[String] = Array("+", "2.1", "3")
-      val sum : Operation = new Sum()
-      sum.usage()
-      assertEquals("sum", sum.getSymbol)
-      assertEquals("5.1", sum.doOperation)
+      implicit val args : Array[String] = Array("sum", "2.1", "3")
+      val output = ops.processOperation
+      assertEquals("5.1", output)
     }
 
   @Test
   def testSub(): Unit ={
-    implicit val args : Array[String] = Array("+", "5.1", "3")
-    val sub : Operation = new Sub()
-    sub.usage()
-    assertEquals("sub", sub.getSymbol)
-    assertEquals("2.1", sub.doOperation)
+    implicit val args : Array[String] = Array("sub", "5.1", "3")
+    val output = ops.processOperation
+    assertEquals("2.1", output)
   }
 
   @Test
   def testMultiplication(): Unit ={
-    implicit val args : Array[String] = Array("*", "5", "3")
-    val prod : Operation = new Prod()
-    prod.usage()
-    assertEquals("mult", prod.getSymbol)
-    assertEquals("15.0", prod.doOperation)
+    implicit val args : Array[String] = Array("mult", "5", "3")
+    val output = ops.processOperation
+    assertEquals("15.0", output)
   }
 }
