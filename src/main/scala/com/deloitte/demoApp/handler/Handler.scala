9be5cf556caf40d656a81806d829c4875f7847dc
rrazumilovs <razumilovrenat@inbox.lv>
2021-12-21 15:31:42 +0200

    handler for ops impl

--- src/main/scala/com/deloitte/demoApp/handler/Handler.scala
+++ src/main/scala/com/deloitte/demoApp/handler/Handler.scala
@@ -1,11 +1,25 @@
 package com.deloitte.demoApp.handler
 
+import com.deloitte.demoApp.operations.Operation
 import org.clapper.classutil.ClassFinder
 
-object Handler extends App {
-  val finder = ClassFinder()
-  val impl = ClassFinder.concreteSubclasses("com.deloitte.demoApp.operations.Operation",
-    finder.getClasses())
+import scala.collection.mutable.ListBuffer
 
-  impl.foreach(println)
+object Handler {
+
+  def getOperations: Map[String, Operation]= {
+    val finder = ClassFinder()
+    val impl = ClassFinder.concreteSubclasses("com.deloitte.demoApp.operations.Operation",
+      finder.getClasses())
+
+    val ops = new ListBuffer[Operation]()
+    for (item <- impl) {
+      ops += Class.forName(item.toString()).newInstance.asInstanceOf[Operation]
+    }
+    toMap(ops.toList)
+  }
+
+  private def toMap(list: List[Operation]): Map[String, Operation] = {
+    Map(list map {op => (op.getSymbol, op)} :_*)
+  }
 }
