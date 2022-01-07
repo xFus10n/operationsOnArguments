c043e89527c3b94db2d07cfaf98b266df713e861
rrazumilovs <razumilovrenat@inbox.lv>
2022-01-07 10:48:06 +0200

    sorting asc desc

--- src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortA.scala
+++ src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortA.scala
@@ -1,14 +1,25 @@
-//package com.deloitte.demoApp.operations.operationImpl.sort
-//
-//import com.deloitte.demoApp.operations.Operation
-//
-//class SortA extends Operation {
-//  override def usage(): Unit = println(getSymbol + "\nSort Asc. = 1 3 2 => 1 2 3")
-//
-//  override def getSymbol: String = "sorta"
-//
-//  override def doOperation(implicit args: Array[String]): String = {
-//    val list : List[String] = args.slice(1, args.length).toList
-//    list.map(x => x.toDouble).sortWith((x,y) => x < y).mkString(" ")
-//  }
-//}
+package com.deloitte.demoApp.operations.operationImpl.sort
+
+import com.deloitte.demoApp.casting.ArgCaster
+import com.deloitte.demoApp.cli.Config
+import com.deloitte.demoApp.operations.Operation
+
+class SortA extends Operation {
+  override def usage(): Unit = println(getSymbol + "\nSort Asc. = 1 3 2 => 1 2 3")
+
+  override def getSymbol: String = "sorta"
+
+  override def doOperation(implicit options: Config): String = {
+    implicit val operands: List[String] = options.operands.toList
+    val typez: String = options.typez
+    val caster = new ArgCaster
+
+    val output: String = typez.toLowerCase match {
+      case "int" => caster.getIntList.sorted.mkString(" ")
+      case "double" => caster.getDoubleList.sorted.mkString(" ")
+      case "long" => caster.getLongList.sorted.mkString(" ")
+    }
+
+    s"$output"
+  }
+}
--- src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortD.scala
+++ src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortD.scala
@@ -1,14 +1,25 @@
-//package com.deloitte.demoApp.operations.operationImpl.sort
-//
-//import com.deloitte.demoApp.operations.Operation
-//
-//class SortD extends Operation {
-//  override def usage(): Unit = println(getSymbol + "\nSort Desc. = 1 3 2 => 3 2 1")
-//
-//  override def getSymbol: String = "sortd"
-//
-//  override def doOperation(implicit args: Array[String]): String = {
-//    val list : List[String] = args.slice(1, args.length).toList
-//    list.map(x => x.toDouble).sortWith((x,y) => x > y).mkString(" ")
-//  }
-//}
+package com.deloitte.demoApp.operations.operationImpl.sort
+
+import com.deloitte.demoApp.casting.ArgCaster
+import com.deloitte.demoApp.cli.Config
+import com.deloitte.demoApp.operations.Operation
+
+class SortD extends Operation {
+  override def usage(): Unit = println(getSymbol + "\nSort Desc. = 1 3 2 => 3 2 1")
+
+  override def getSymbol: String = "sortd"
+
+  override def doOperation(implicit options: Config): String = {
+    implicit val operands: List[String] = options.operands.toList
+    val typez: String = options.typez
+    val caster = new ArgCaster
+
+    val output: String = typez.toLowerCase match {
+      case "int" => caster.getIntList.sorted(Ordering.Int.reverse).mkString(" ")
+      case "double" => caster.getDoubleList.sorted(Ordering.Double.TotalOrdering.reverse).mkString(" ")
+      case "long" => caster.getLongList.sorted(Ordering.Long.reverse).mkString(" ")
+    }
+
+    s"$output"
+  }
+}
