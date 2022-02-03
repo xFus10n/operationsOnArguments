569c88b699cd1c480da4fbb491a95e9cc1c1bed5
rrazumilovs <razumilovrenat@inbox.lv>
2022-02-03 09:02:12 +0200

    quick sort debug msg

--- src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortQ.scala
+++ src/main/scala/com/deloitte/demoApp/operations/operationImpl/sort/SortQ.scala
@@ -29,51 +29,52 @@ class SortQ extends Operation{
 object SortQ {
 
   /** Performs recursive quicksort on an array
-   *
-   * There's an opportunity for limiting space complexity to
-   * O(ln(n)) here if we recurse on the larger partition last
-   * (tail recursion).
-   *  */
+   */
   def quicksort[T](array: Array[T], low: Int, hi: Int)(implicit ev: T => Ordered[T]): Array[T] = {
+    println("----------------------------------------------")
+    println(array.mkString(" ") + s" low = $low, high = $hi")
     if (low < hi) {
       val p = partition(array, low, hi)
       quicksort(array, low, p-1)
       quicksort(array, p+1, hi)
     }
+    array
+  }
 
-    /** Partition an array around some pivot
-     *
-     *  Chooses a pivot, moves all values less than
-     *  the pivot to its left, and moves all values
-     *  greater than the pivot to its right.
-     *
-     *  To optimize: choose pivot corresponding to a median
-     *  value of some sample. This helps ensure the
-     *  partitioning results in similarly-sized sub-arrays,
-     *  and thus the optimal O(n*ln(n)) performance.
-     *
-     *  @return index (location) of pivot in partitioned array
-     */
-    def partition[A](subArray: Array[A], low: Int, hi: Int)(implicit ev: A => Ordered[A]): Int = {
-      val pivot = hi
-      var i = low
-      for (
-        j <- low to hi
-        if subArray(j) < subArray(pivot)
-      ) {swap(subArray, i, j); i+=1}
-
-      // Lastly, move pivot value to the dividing line at i
-      swap(subArray, i, pivot)
-      i
+  /** Partition an array around some pivot
+   *
+   *  Chooses a pivot, moves all values less than
+   *  the pivot to its left, and moves all values
+   *  greater than the pivot to its right.
+   *
+   *  To optimize: choose pivot corresponding to a median
+   *  value of some sample. This helps ensure the
+   *  partitioning results in similarly-sized sub-arrays,
+   *  and thus the optimal O(n*ln(n)) performance.
+   *
+   *  @return index (location) of pivot in partitioned array
+   */
+  def partition[A](subArray: Array[A], low: Int, hi: Int)(implicit ev: A => Ordered[A]): Int = {
+    val pivot = hi
+    var lowTmp = low
+    for (j <- low to hi if subArray(j) < subArray(pivot)) {
+      println("swap before: " + subArray.mkString(" ") + s" low=$lowTmp, element=$j,${subArray(j)} < pivot=$pivot,${subArray(pivot)}")
+      swap(subArray, lowTmp, j)
+      lowTmp += 1
+      println("swap after: " + subArray.mkString(" ") + s" low=$lowTmp, element=$j,${subArray(j)} pivot=$pivot,${subArray(pivot)}")
     }
 
-    array
+    // Lastly, move pivot value to the dividing line at i
+    swap(subArray, lowTmp, pivot)
+    lowTmp
   }
 
   /** Swaps two values in an array */
   def swap[T](array: Array[T], pos1: Int, pos2: Int): Unit = {
+    if (pos1==pos2) return
     val stash = array(pos1)
     array(pos1) = array(pos2)
     array(pos2) = stash
+    println("swap : " + array.mkString(" ") + s" pos1 = $pos1 pos2 = $pos2")
   }
 }
